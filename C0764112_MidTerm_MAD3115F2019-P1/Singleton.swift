6556134b4e40556acc72f13c5da2f5dd60fc5c9a
Ankita <ankita.jain12342@gmail.com>
2019-11-01 15:08:15 -0400

    singleton class added

---
+++ C0764112_MidTerm_MAD3115F2019-P1/Singleton.swift
@@ -0,0 +1,33 @@
+//
+//  Singleton.swift
+//  C0764112_MidTerm_MAD3115F2019-P1
+//
+//  Created by Sandeep Jangra on 2019-11-01.
+//  Copyright © 2019 Sandeep Singh. All rights reserved.
+//
+
+import Foundation
+
+class Singleton: NSObject
+    
+{
+     
+    private var customerDictionary  = [Int:Customer]()
+    
+   
+    private static var obj = Singleton()
+    
+    
+    private override init() {
+        
+    }
+    
+    
+    internal static func getInstance() -> Singleton
+        
+    {
+        
+        return obj
+    }
+    
+   
