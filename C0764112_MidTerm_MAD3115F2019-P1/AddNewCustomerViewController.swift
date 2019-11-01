e9f12a6c676fe034945bfd28d4efad1ae77789ee
Ankita <ankita.jain12342@gmail.com>
2019-11-01 16:39:40 -0400

    add  new customer class created

---
+++ C0764112_MidTerm_MAD3115F2019-P1/AddNewCustomerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddNewCustomerViewController.swift
+//  C0764112_MidTerm_MAD3115F2019-P1
+//
+//  Created by Sandeep Jangra on 2019-11-01.
+//  Copyright © 2019 Sandeep Singh. All rights reserved.
+//
+
+import UIKit
+
+class AddNewCustomerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
