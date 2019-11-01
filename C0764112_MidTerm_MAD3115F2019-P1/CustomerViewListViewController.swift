39fcc276e54a0bc01d957439aec4655bc03434c0
Ankita <ankita.jain12342@gmail.com>
2019-11-01 15:36:52 -0400

    override func done in class

--- C0764112_MidTerm_MAD3115F2019-P1/CustomerViewListViewController.swift
+++ C0764112_MidTerm_MAD3115F2019-P1/CustomerViewListViewController.swift
@@ -88,4 +88,31 @@ func logout(sender: UIBarButtonItem)
     
 }
 
-   
+    override func viewDidLoad()
+       {
+           super.viewDidLoad()
+           
+           customerListTable.delegate=self
+           
+             customerListTable.dataSource=self
+           
+           navigationItem.hidesBackButton=true
+           
+           
+              addLogoutButton()
+           // Do any additional setup after loading the view.
+       }
+       
+
+       /*
+       // MARK: - Navigation
+
+       // In a storyboard-based application, you will often want to do a little preparation before navigation
+       override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+           // Get the new view controller using segue.destination.
+           // Pass the selected object to the new view controller.
+       }
+       */
+
+   }
+
